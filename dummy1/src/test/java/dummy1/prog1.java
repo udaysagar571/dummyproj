fa00788b4013cd6bebf1bfeb57684d4b859b3ff3
USER <USER@DESKTOP-9M8A2RP>
2025-11-17 15:15:18 +0530

    This is first commit

---
+++ dummy1/src/test/java/dummy1/prog1.java
@@ -0,0 +1,13 @@
+package dummy1;
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("every one");
+
+	}
+
+}
